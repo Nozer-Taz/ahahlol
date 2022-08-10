41593cec68aa4bb648d526421e615bfc3dcb5d36
Nazar <111nazarbektajibaev@gmail.com>
2022-08-11 00:18:58 +0600

    ch(deploy)

--- kahoot/settings.py
+++ kahoot/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -166,3 +168,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+django_heroku.settings(locals())
